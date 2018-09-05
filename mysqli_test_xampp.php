1009d9d6cbb9c0e22154f24d4e9ca3245eecb1bf
owenwhenry <henryowenw@gmail.com>
2018-09-04 20:44:53 -0400

    Test files for XAMPP

---
+++ mysqli_test_xampp.php
@@ -0,0 +1,9 @@
+<?php
+$dbc = @mysqli_connect ('localhost','root','','test') OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+$query="SELECT COUNT(*) FROM information_schema.CHARACTER_SETS C;";
+$result = @mysqli_query ($dbc, $query);
+$row = mysqli_fetch_array($result, MYSQLI_NUM);
+$somet=$row[0];
+echo $somet;
+?>
+
---
+++ test_errors_on.php
@@ -0,0 +1,5 @@
+<?php
+//Introduces an error to check whether the display_errors option in php.ini is ON.
+//Must have '; at the end of the line.
+echo 'Hi!
+?>
